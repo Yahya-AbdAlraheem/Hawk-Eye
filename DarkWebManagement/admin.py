24dad164766c1d3a56635c7acbfafdb2c6536aa2
Yahya-AbdAlraheem <yahyaabd625@gmail.com>
2025-03-04 03:49:48 +0300

    Update admin.py

--- DarkWebManagement/admin.py
+++ DarkWebManagement/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from DarkWebManagement.models import ExtractedData
+
 # Register your models here.
+
+admin.site.register(ExtractedData)
+admin.site.site_header='Database Administrator'
+admin.site.site_title='Hawk Eye'
