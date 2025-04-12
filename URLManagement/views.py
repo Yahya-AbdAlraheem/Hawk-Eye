82ce975a50c13d8e3d337c8c1f2a39b0260f357d
Yahya-AbdAlraheem <yahyaabd625@gmail.com>
2025-04-12 22:54:43 +0300

    Update views.py

--- URLManagement/views.py
+++ URLManagement/views.py
@@ -3,6 +3,7 @@ import requests
 import base64
 import os
 from django.conf import settings
+from datetime import datetime
 
 VT_API_KEY = os.environ.get('VT_API_KEY')
 if not VT_API_KEY:
@@ -29,9 +30,39 @@ def check_url(request):
                 vt_data = response.json()
                 context['result'] = vt_data
                 context['url'] = url_to_check
+
+                # جلب التاريخ وتحويله إلى الشكل: April 12, 2025, 6:27 p.m.
+                scan_timestamp = vt_data['data']['attributes'].get('last_analysis_date')
+                if scan_timestamp:
+                    scan_date = datetime.fromtimestamp(scan_timestamp).strftime('%B %d, %Y, %I:%M %p')
+                    scan_date = scan_date.replace("AM", "a.m.").replace("PM", "p.m.")
+                    context['scan_date'] = scan_date
+
+                # حساب النسبة المئوية للمخاطر
+                malicious = vt_data['data']['attributes']['last_analysis_stats']['malicious']
+                suspicious = vt_data['data']['attributes']['last_analysis_stats']['suspicious']
+                undetected = vt_data['data']['attributes']['last_analysis_stats']['undetected']
+                harmless = vt_data['data']['attributes']['last_analysis_stats']['harmless']
+
+                total = malicious + suspicious + undetected + harmless
+                risk_value = 0
+
+                if total > 0:
+                    # حساب النسبة المئوية للمخاطر
+                    risk_value = (malicious + suspicious) / total * 100
+
+                # إضافة النسبة المئوية إلى context
+                context['risk_value'] = round(risk_value, 2)
+
+                # تصنيف المخاطر
+                if risk_value > 70:
+                    context['risk_class'] = 'malicious'
+                elif risk_value > 30:
+                    context['risk_class'] = 'medium-risk'
+                else:
+                    context['risk_class'] = 'harmless'
+
             else:
-                context['error'] = 'فشل في جلب البيانات من VirusTotal.'
+                context['error'] = 'Failed to Fetch Data From VirusTotal.'
 
     return render(request, 'pages/Check_URL.html', context)
-
-
