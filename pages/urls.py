fdee5b9c692d210d72a0e1a2bcb80bdaa20f6a0f
Yahya-AbdAlraheem <yahyaabd625@gmail.com>
2025-04-14 23:00:16 +0300

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.WelcomePage, name='WelcomePage'),  
     path('hawk-eye/', views.HomePage, name='HomePage'),
-    path('create-password/', views.CreatePassword, name='CreatePassword'),
+    path('check-password/', views.CreatePassword, name='CreatePassword'),
     path('about-us/', views.AboutUs, name='AboutUs'),
-    path('contact/', views.Contact, name='Contact')
+    path('contact/', views.Contact, name='Contact'),
+    path('whois_lookup/', views.whois_lookup, name='whois_lookup'),
 ]
